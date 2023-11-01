dc9af8758f7f37a0008f276b0c35c255d02b1987
Ahmad Jayossi <ahjayossi@gmail.com>
2023-11-01 17:53:16 +0100

    test how shadcn works by making button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-      <h1 className="text-red-600">Home</h1>
+    <Button variant="default" size="lg">Hello</Button>      
   )
 }
