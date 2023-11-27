2577102553ccf9d01cfdcd507d02e1f4432625f5
Ahmad Jayossi <ahjayossi@gmail.com>
2023-11-27 15:39:07 +0100

    partial serialize need work

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 // TypeScript users only add this code
@@ -8,18 +8,16 @@ import { Descendant } from "slate";
 import { Leaf, Element } from "./Elements";
 import { ToolBar, toolbarIcons, onKeyDown, CEditor } from "./utils";
 import { withHistory } from "slate-history";
-import { initialResumeTemplate } from "./template";
-
-const initialValue: Descendant[] = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-    align: "left",
-  },
-];
+import "./richtexteditor.css";
+import { serialize } from "./serializer";
 
 const RichTextEditor = () => {
   const [editor] = useState(() => withReact(withHistory(createEditor())));
+  const storedContent = localStorage.getItem("content");
+  const initialResumeTemplate: Descendant[] = useMemo(
+    () => (storedContent ? JSON.parse(storedContent) : initialResumeTemplate),
+    []
+  );
 
   const renderElement = useCallback((props: any) => {
     return <Element {...props} />;
@@ -28,18 +26,30 @@ const RichTextEditor = () => {
     return <Leaf {...props} />;
   }, []);
   return (
-    <Slate editor={editor} initialValue={initialResumeTemplate}>
-      <div>
-        <ToolBar>{toolbarIcons(editor)}</ToolBar>
-      </div>
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={(event) => onKeyDown(event, editor)}
-      />
-    </Slate>
+    <div className="editable-container">
+      <Slate editor={editor} initialValue={initialResumeTemplate}>
+        <div className="toolbar-container">
+          <ToolBar>{toolbarIcons(editor)}</ToolBar>
+        </div>
+        <Editable
+          className="editor-container"
+          renderElement={renderElement}
+          renderLeaf={renderLeaf}
+          onKeyDown={(event) => onKeyDown(event, editor)}
+          onChange={(value) => {
+            const isAstChange = editor.operations.some(
+              (op) => "set_selection" !== op.type
+            );
+            if (isAstChange) {
+              // Save the value to Local Storage.
+              const content = serialize(value);
+              localStorage.setItem("content", content);
+            }
+          }}
+        />
+      </Slate>
+    </div>
   );
 };
 
 export default RichTextEditor;
-
