64a6fb11483dbeaef85a4dd3790bd8d27d284398
Ahmad Jayossi <ahjayossi@gmail.com>
2023-11-29 12:29:39 +0100

    HTML serializer in RichTextEditor

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -9,25 +9,43 @@ import { Leaf, Element } from "./Elements";
 import { ToolBar, toolbarIcons, onKeyDown, CEditor } from "./utils";
 import { withHistory } from "slate-history";
 import "./richtexteditor.css";
-import { serialize } from "./serializer";
+import { initialResumeTemplate } from "./template";
+import { serializeHTML, deserializeHTML } from "./serializer";
 
 const RichTextEditor = () => {
   const [editor] = useState(() => withReact(withHistory(createEditor())));
-  const storedContent = localStorage.getItem("content");
-  const initialResumeTemplate: Descendant[] = useMemo(
-    () => (storedContent ? JSON.parse(storedContent) : initialResumeTemplate),
-    []
-  );
-
+  const initialValue: Descendant[] = useMemo(() => {
+    const storedContent = localStorage.getItem("content");
+    const content = storedContent
+      ? deserializeHTML(storedContent)
+      : initialResumeTemplate;
+  
+    return content;
+  }, []);
+  
   const renderElement = useCallback((props: any) => {
     return <Element {...props} />;
   }, []);
   const renderLeaf = useCallback((props: any) => {
     return <Leaf {...props} />;
   }, []);
+  1;
   return (
     <div className="editable-container">
-      <Slate editor={editor} initialValue={initialResumeTemplate}>
+      <Slate
+        editor={editor}
+        initialValue={initialValue}
+        onChange={(value) => {
+          //meaning that the user has changed the content of editor except for the selection
+          const isAstChange = editor.operations.some(
+            (op) => "set_selection" !== op.type
+          );
+          if (isAstChange) {
+            const content = serializeHTML(value);
+            localStorage.setItem("content", content);
+          }
+        }}
+      >
         <div className="toolbar-container">
           <ToolBar>{toolbarIcons(editor)}</ToolBar>
         </div>
@@ -36,16 +54,6 @@ const RichTextEditor = () => {
           renderElement={renderElement}
           renderLeaf={renderLeaf}
           onKeyDown={(event) => onKeyDown(event, editor)}
-          onChange={(value) => {
-            const isAstChange = editor.operations.some(
-              (op) => "set_selection" !== op.type
-            );
-            if (isAstChange) {
-              // Save the value to Local Storage.
-              const content = serialize(value);
-              localStorage.setItem("content", content);
-            }
-          }}
         />
       </Slate>
     </div>
--- src/components/RichTextEditor/serializer.tsx
+++ src/components/RichTextEditor/serializer.tsx
@@ -1,23 +1,135 @@
 // Import the `Node` helper interface from Slate.
-import { Node } from 'slate'
+import {  Element as SlateElement } from "slate";
+import { Descendant } from "slate";
+import { Text } from "slate";
+import { jsx } from "slate-hyperscript";
+import escapeHtml from "escape-html";
+import { CustomElement } from "./types";
 
-// Define a serializing function that takes a value and returns a string.
- export  const serialize = (value: HTMLDivElement[]) => {
+export const serializeHTML = (node: any) => {
+  if (Text.isText(node)) {
+    let string = escapeHtml(node.text);
+    if (node.bold) string = `<strong>${string}</strong>`;
+    if (node.italic) string = `<em>${string}</em>`;
+    if (node.underline) string = `<u>${string}</u>`;
+
+    return string;
+  }
+  const children: string = node.children.map((n: any) => serializeHTML(n)).join("");
+  switch (node.type) {
+    case "paragraph":
+      return `<p>${children}</p>`;
+    case "heading":
+      return `<h1>${children}</h1>`;
+    case "subheading":
+      return `<h2>${children}</h2>`;
+    case "list":
+      return `<ul>${children}</ul>`;
+    case "bulletpoint":
+      return `<li>${children}</li>`;
+    case "title":
+      return `<h1>${children}</h1>`;
+    case "subtitle":
+      return `<h2>${children}</h2>`;
+    case "section":
+      return `<div>${children}</div>`;
+    case "section_title":
+      return `<h3>${children}</h3>`;
+    case "section_subtitle":
+      return `<h4>${children}</h4>`;
+    case "subsection_title":
+      return `<h4>${children}</h4>`;
+    case "subsection_subtitle":
+      return `<h5>${children}</h5>`;
+    default:
+      return children;
+  }
+};
+
+export const deserializeHTML = (el: any, markAttributes: any = {}): any => {
+  if (el.nodeType === Node.TEXT_NODE) {
+    return jsx("text", markAttributes, el.textContent);
+  } else if (el.nodeType !== Node.ELEMENT_NODE) {
+    return null;
+  }
+
+  const nodeAttributes = { ...markAttributes };
+
+  // define attributes for text nodes
+  switch (el.nodeName) {
+    case "STRONG":
+      nodeAttributes.bold = true;
+  }
+
+  const children = Array.from(el.childNodes)
+    .map((node) => deserializeHTML(node, nodeAttributes))
+    .flat();
+
+  if (children.length === 0) {
+    children.push(jsx("text", nodeAttributes, ""));
+  }
+
+  switch (el.nodeName) {
+    case "BODY":
+      return jsx("fragment", {}, children);
+    case "BR":
+      return "\n";
+    case "BLOCKQUOTE":
+      return jsx("element", { type: "quote" }, children);
+    case "P":
+      return jsx("element", { type: "paragraph" }, children);
+    case "A":
+      return jsx(
+        "element",
+        { type: "link", url: el.getAttribute("href") },
+        children
+      );
+    case "STRONG":
+      return jsx("text", { bold: true }, children);
+    case "EM":
+      return jsx("text", { italic: true }, children);
+    case "U":
+      return jsx("text", { underline: true }, children);
+    case "UL":
+      return jsx("element", { type: "list" }, children);
+    case "LI":
+      return jsx("element", { type: "bulletpoint" }, children);
+    case "H1":
+      return jsx("element", { type: "title" }, children);
+    case "H2":
+      return jsx("element", { type: "subtitle" }, children);
+    case "DIV":
+      return jsx("element", { type: "section" }, children);
+    case "H3":
+      return jsx("element", { type: "section_title" }, children);
+    case "H4":
+      return jsx("element", { type: "section_subtitle" }, children);
+    case "H4":
+      return jsx("element", { type: "subsection_title" }, children);
+    case "H5":
+      return jsx("element", { type: "subsection_subtitle" }, children);
+    default:
+      return children;
+  }
+};
+/* // Define a serializing function that takes a value and returns a string.
+export const serializePlain = (value: any) => {
   return (
     value
       // Return the string content of each paragraph in the value's children.
-      .map(n => Node.string(n))
+      .map((n: any) => Node.string(n))
       // Join them all with line breaks denoting paragraphs.
-      .join('\n')
-  )
-}
+      .join("\n")
+  );
+};
 
 // Define a deserializing function that takes a string and returns a value.
-export const deserialize = (string: string) => {
+export const deserializePlain = (string: string): any => {
   // Return a value array of children derived by splitting the string.
-  return string.split('\n').map(line => {
+  return string.split("\n").map((line) => {
     return {
       children: [{ text: line }],
-    }
-  })
-}
+    };
+  });
+};
+ */
